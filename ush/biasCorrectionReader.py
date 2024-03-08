6b4a30e4a77876f09da0e053902fb4a3a8e09fdf
Kevin Dougherty <kevin.dougherty@noaa.gov>
2024-03-08 18:26:23 +0000

    updated bias correction reader script

--- ush/biasCorrectionReader.py
+++ ush/biasCorrectionReader.py
@@ -1,4 +1,3 @@
-import argparse
 import yaml
 from netCDF4 import Dataset
 import numpy as np
@@ -6,7 +5,7 @@ import netCDF4 as nc
 from datetime import datetime
 
 
-def read_ncfile(file, group_list):
+def read_ncfile(file, groups, variable):
     """
     Read and extract data from ncfile based on inputted groups.
 
@@ -19,83 +18,20 @@ def read_ncfile(file, group_list):
     """
     d = {}
 
-    with Dataset(file, mode='r') as f:
-        for g in group_list:
-            gd = f.groups[g]
+    try:
+        with Dataset(file, mode='r') as f:
+            for g in groups:
+                d[g] = {}
+                gd = f.groups[g]
 
-            d[g] = {}
+                data = gd.variables[variable][:]
+                d[g][variable] = data
 
-            for var in gd.variables:
-                data = gd.variables[var][:]
-                d[g][var] = data
+    except KeyError as e:
+        print(f"Extraction of data failed. {e} does not exist in .ncfile ")
+        # Force exit
 
     return d
-
-
-def calculate_omf(data_dict, hofx, variable, biascorrection=True, obsbias=None):
-    """
-    Calculate observation minus forecast for data that is bias corrected and not bias corrected.
-
-    Parameters:
-        data_dict (dict): dictionary of data from input nc file
-        hofx (str): the hofx variable to be used (i.e. hofx0, hofx1)
-        variable (str): group variable specified in input yaml file
-        biascorrection (bool): if using bias corrected data, set to True. If using no bias
-                               corrected data, set to False
-        obsbias (str): the obs bias variable to be used (i.e. ObsBias0, ObsBias1). Only needed
-                       if biascorrection=False
-    Return:
-        omf (array): the observation minus forecast result
-    """
-    try:
-        omf = data_dict['ObsValue'][variable] - data_dict[hofx][variable]
-
-        if biascorrection:
-            return omf
-        else:
-            return omf - data_dict[obsbias][variable]
-
-    except KeyError as error:
-        print(f"Calculation failed. Missing input group variable: {error}")
-
-
-def calculate_penalty(data_dict, omf, obsbias, variable):
-    """
-    Calculates penalty by observation minus forecast by effective error.
-
-    Parameters:
-        data_dict (dict):  dictionary of data from input nc file
-        omf (array): observation minus forecast array
-        obsbias (str): the obs bias variable to be used (i.e. ObsBias0, ObsBias1)
-        variable (str): group variable specified in input yaml file
-    Return:
-        penalty (array): observation minus forecast divided by observation bias array
-    """
-    try:
-        penalty = omf / data_dict[obsbias][variable]
-
-        return penalty
-
-    except KeyError as error:
-        print(f"Calculation failed. Missing input group variable: {error}")
-
-
-def datetime2epoch(cycle):
-    """
-    Convert a datetime in str to epoch time.
-
-    Parameters:
-        cycle (str): date of cycle in YYYYMMDDHH
-    Return:
-        epoch_time (int): value of time in seconds since Jan. 1, 1970
-    """
-    # Convert string to datetime object
-    dt_object = datetime.strptime(cycle, '%Y%m%d%H')
-
-    # Convert datetime object to Unix epoch time (seconds since January 1, 1970)
-    epoch_time = [int(dt_object.timestamp())]
-
-    return epoch_time
 
 
 def write_ncfile(outfile, outdata, epoch_time, nchannels):
@@ -141,63 +77,184 @@ def write_ncfile(outfile, outdata, epoch_time, nchannels):
     return
 
 
-def main(input_file, cycle, satellite, channels, outvars, outfile, group_names, group_variable):
+def datetime2epoch(cycle):
     """
-    Read in config file contaiting bias corrected data, calculate counts, averages, and standard
+    Convert a datetime in str to epoch time.
+
+    Parameters:
+        cycle (str): date of cycle in YYYYMMDDHH
+    Return:
+        epoch_time (int): value of time in seconds since Jan. 1, 1970
+    """
+    # Convert string to datetime object
+    dt_object = datetime.strptime(cycle, '%Y%m%d%H')
+
+    # Convert datetime object to Unix epoch time (seconds since January 1, 1970)
+    epoch_time = [int(dt_object.timestamp())]
+
+    return epoch_time
+
+
+def calculate_omf(filename, groups, variable, channels, outgroups, bias_corr, bias_groups):
+    """
+    Calculate observation minus forecast for data that is bias corrected and not bias corrected.
+
+    Parameters:
+        filename (str): input path and filename to netCDF file to be read
+        groups (list): list of strings that contains group names from input .nc file
+        variable (str): variable within input .nc file groups
+        channels (array): channels where data should be grabbed
+        outgroups (list): list of strings that contains the group names for the output .nc file
+        bias_corr (bool): boolean value to determine if bias correction was used or not
+        bias_groups (list): list of observation bias values to be used from input .nc file
+    Return:
+        return_dict (dict): dictionary of observation minus forecast values
+    """
+    # Add 'ObsValue' to groups to calculate omf
+    groups.append('ObsValue')
+
+    # Grab data from .nc file
+    data_dict = read_ncfile(filename, groups, variable)
+
+    # Calculate omf
+    return_dict = {}
+
+    for i, output in enumerate(outgroups):
+        return_dict[output] = {}
+
+        hofx = groups[i]
+        omf = data_dict['ObsValue'][variable] - data_dict[hofx][variable]
+
+        if bias_corr:
+            return_dict[output]['data'] = omf
+        else:
+            bias_dict = read_ncfile(filename, bias_groups, variable)
+            obsbias = bias_groups[i]
+
+            return_dict[output]['data'] = omf - bias_dict[obsbias][variable]
+
+    return return_dict
+
+
+def calculate_penalty(filename, groups, variable, channels, outgroups):
+    """
+    Calculates penalty by observation minus forecast by effective error.
+
+    Parameters:
+        filename (str): input path and filename to netCDF file to be read
+        groups (list): list of strings that contains group names from input .nc file
+        variable (str): variable within input .nc file groups
+        channels (array): channels where data should be grabbed
+        outgroups (list): list of strings that contains the group names for the output .nc file
+    Return:
+        return_dict (dict): dictionary of penalty values
+    """
+    return_dict = {}
+
+    omf_dict = calculate_omf(filename, groups, variable, channels, outgroups, bias_corr=True, bias_groups=None)
+
+    # logic check to see how many effective errors you need?
+    efferr = ['EffectiveError0', 'EffectiveError1']
+
+    efferr_dict = read_ncfile(filename, efferr, variable)
+
+    for i, output in enumerate(outgroups):
+        return_dict[output] = {}
+
+        for k, key in enumerate(omf_dict.keys()):
+
+            return_dict[output]['data'] = omf_dict[key]['data'] / efferr_dict[efferr[k]][variable]
+
+    return return_dict
+
+
+def grab_data(filename, groups, variable, channels, outgroups):
+    """
+    Grabs data from input .nc file
+
+    Paramaters:
+        filename (str): input path and filename to netCDF file to be read
+        groups (list): list of strings that contains group names from input .nc file
+        variable (str): variable within input .nc file groups
+        channels (array): channels where data should be grabbed
+        outgroups (list): list of strings that contains the group names for the output .nc file
+    Return:
+        return_dict (dict): dictionary of data values
+    """
+    return_dict = {}
+
+    for i, group in enumerate(groups):
+        outgroup_key = outgroups[i]
+        return_dict[outgroup_key] = {}
+
+        data_dict = read_ncfile(filename, [group], variable)
+
+        return_dict[outgroup_key]['data'] = data_dict[group][variable]
+
+    return return_dict
+
+
+def main(filename, cycle, satellite, config_data, outfile):
+    """
+    Read in JEDI diagnostic config file, calculate counts, averages, and standard
     deviation, and output results to a new netCDF file with time information.
 
     Parameters:
-        input_file (str): input path and filename to netCDF file to be read
-        cycle (str): cycle in YYYYMMDDHH
+        filename (str): input path and filename to netCDF file to be read
+        cycle (str): forecast run cycle in YYYYMMDDHH
         satellite (str): name of satellite from input data
-        channels (str): channels where data should be grabbed
-        outvars (list): list of strings of the desired variables to be saved to new .nc file
-        outfile (file): path and filename for new .nc file
-        group_names (list): list of strings that contains group names from input .nc file
-        group_variable (str): variable within input .nc file groups
+        config_data (dict): dictionary of related information pulled from input yaml file
+        outfile (str): path and filename for new .nc file
     Return:
         None
     """
-    # Grab config info
-    data_dict = read_ncfile(input_file, group_names)
 
-    # Can this block be better??
-    omgbc0 = calculate_omf(data_dict, hofx='hofx0', variable=group_variable)
-    omgbc1 = calculate_omf(data_dict, hofx='hofx1', variable=group_variable)
-    omgnbc0 = calculate_omf(data_dict, hofx='hofx0', variable=group_variable,
-                            biascorrection=False, obsbias='ObsBias0')
-    omgnbc1 = calculate_omf(data_dict, hofx='hofx1', variable=group_variable,
-                            biascorrection=False, obsbias='ObsBias1')
-    penalty0 = calculate_penalty(data_dict, omgbc0, input1='ObsBias0', variable=group_variable)
-    penalty1 = calculate_penalty(data_dict, omgbc1, input1='ObsBias1', variable=group_variable)
-    obsbias0 = data_dict['ObsBias0'][group_variable]
-    obsbias1 = data_dict['ObsBias1'][group_variable]
-    lapserate1 = data_dict['lapse_ratePredictor'][group_variable]
-    lapserate2 = data_dict['lapse_rate_order_2Predictor'][group_variable]
-    constant = data_dict['constantPredictor'][group_variable]
-    emissivity = data_dict['emissivityPredictor'][group_variable]
-    scanangle = data_dict['scan_anglePredictor'][group_variable]
-    scanangle2 = data_dict['scan_angle_order_2Predictor'][group_variable]
-    scanangle3 = data_dict['scan_angle_order_3Predictor'][group_variable]
-    scanangle4 = data_dict['scan_angle_order_4Predictor'][group_variable]
+    factory = {
+        'calculate_omf': calculate_omf,
+        'calculate_penalty': calculate_penalty,
+        'grab_data': grab_data
+    }
 
-    data_list = [omgbc0, omgbc1, omgnbc0, omgnbc1, penalty0, penalty1,
-                 obsbias0, obsbias1, lapserate1, lapserate2, constant,
-                 emissivity, scanangle, scanangle2, scanangle3, scanangle4]
+    outdata = {}
 
-    #########################################################################
+    for inputdict in config_data:
+        function = inputdict.get('function')
+        groups = inputdict.get('groups')
+        variable = inputdict.get('variable')
+        channels = inputdict.get('channels')
+        out_groups = inputdict.get('groups out')
 
-    # Create outdata dictionary
-    outdata = {i: {} for i in outvars}
+        # I think I am going to need a logic check
+        # i.e. len(groups) == len(groups out)
+        # if calling penalty, omf needs to exist? Have same inputs
+        # as `calculate_omf` and then just call omf, and then subtract
+        # effective error?
 
+        if 'bias corrected' in inputdict.keys():
+            bc_dict = inputdict.get('bias corrected')[0]
+
+            bias_corr = bc_dict.get('bias correction used')
+            bias_groups = bc_dict.get('bias groups')
+
+            data = factory[function](filename, groups, variable, channels, out_groups, bias_corr, bias_groups)
+
+        else:
+            data = factory[function](filename, groups, variable, channels, out_groups)
+
+        outdata.update(data)
+        
     # Loop through data to clean data and calculate counts, mean, and std
-    for i, data in enumerate(data_list):
-        # Get list of out dict keys
-        key = list(outdata.keys())[i]
+    for key in outdata.keys():
+
+        data = outdata[key]['data']
 
         # Find and replace bad values with nans
-        extreme_indices = np.where((data > 1e6) | (data < -1e6))
-        data[extreme_indices] = np.nan
+        # This is tricky: How can I do this for the correct EffectiveQC value?
+        # What is the correct QC value?
+        effective_qc = read_ncfile(filename, ['EffectiveQC0'], variable)
+        qc_indices = np.where(effective_qc['EffectiveQC0']['brightnessTemperature'] != 0)
+
+        data[qc_indices] = np.nan
 
         # Calculate counts, mean, standard deviation not including nans
         counts = np.count_nonzero(~np.isnan(data), axis=0)
@@ -207,6 +264,9 @@ def main(input_file, cycle, satellite, channels, outvars, outfile, group_names, 
         outdata[key]['count'] = counts
         outdata[key]['mean'] = mean
         outdata[key]['std'] = std
+
+        # Remove data from outdata dictionary
+        outdata[key].pop('data', None)
 
     # Get epoch time
     epoch_time = np.array(datetime2epoch(cycle))
@@ -233,21 +293,12 @@ if __name__ == "__main__":
         with open(infile, 'r') as infile_opened:
             config_dict = yaml.safe_load(infile_opened)
     except Exception as e:
-        print('dead')
+        print(e)
 
     for data in config_dict.get('datasets'):
         satellite = data.get('satellite')
         filename = data.get('filename')
-        channels = data.get('channels')
-        outvars = data.get('outvars')
+        config_data = data.get('variables to process')
         outfile = data.get('outfile')
 
-        for group in data.get('groups'):
-            group_names = group.get('names')
-            # This will need to be tested in future
-            if satellite == 'ssmis':
-                group_names.append('cos', 'sin')
-            group_variable = group.get('variable')
-
-            main(filename, cycle, satellite, channels,
-                 outvars, outfile, group_names, group_variable)
+    main(filename, cycle, satellite, config_data, outfile)
