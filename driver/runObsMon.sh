8ebd2d80b88fe4b3d54b1ae731fa4b028f0f3c1c
edward.safford <edward.safford@noaa.gov>
2024-04-10 13:15:58 +0000

    Ref #21
    Clean up runObsMon.

--- driver/runObsMon.sh
+++ driver/runObsMon.sh
@@ -97,24 +97,13 @@ if [[ -e ${logfile} ]]; then rm ${logfile}; fi
 
 case ${MACHINE_ID} in
    hera)
-      # Note: Will have to subdivide the plot runs.  Wall times to plot all the radiance
-      #       time-series are taking 1:45:00 now.  That doesn't include Rad bcoef and angle,
-      #       let alone Ozn, Con, and Min plots.  I'm thinking of making separate templates
-      #       for groups of plots that can run in < 2 hrs.  I tried adding additional tasks,
-      #       which does improve runtimes but more than 2 tasks can mean quite a wait for
-      #       queues on hera, while there's almost never a delay to run on the service
-      #       partition.
-#      ${SUB} --account ${ACCOUNT}  --ntasks=1 --mem=40000M --time=3:00:00 \
-#             -J ${jobname} --partition service -o ${logfile} ${jobfile}
-#      ;;
       ${SUB} --account ${ACCOUNT}  --ntasks=1 --mem=400M --time=0:05:00 \
              -J ${jobname} --partition service -o ${logfile} ${jobfile}
       ;;
 
    wcoss2)	# NOTE:  this has not been tested; eva doesn't yet run on wcoss2
-      echo "submitting job on wcoss2"
       $SUB -q $JOB_QUEUE -A $ACCOUNT -o ${logfile} -e ${logfile} \
-           -V -l select=1:mem=500M -l walltime=3:00:00 -N ${jobname} ${jobfile}
+           -V -l select=1:mem=500M -l walltime=0:05:00 -N ${jobname} ${jobfile}
       ;;
 esac
 
