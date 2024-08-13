d72d780ef45b6b5ca9fb8578c410aa9e77e182a5
Edward.Safford <edward.safford@noaa.gov>
2024-08-13 16:03:36 +0000

    Ref #41
    Add module to load venv for obs-monitor.

--- ush/plot_wcoss2.sh
+++ ush/plot_wcoss2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 module reset
-module list
 
 module load libfabric/1.11.0.0.
 module load PrgEnv-intel/8.3.3
@@ -14,12 +13,11 @@ module load git/2.29.0
 module load intel/19.1.3.304
 module load netcdf/4.7.4
 module load python/3.10.4
-module load ve/evs/1.0
+
+module use ${HOMEobsmon}/modulefiles
+module load obs-monitor
 
 module list
-
-export PATH=/lfs/h2/emc/da/noscrub/edward.safford/eva/opt/bin:${PATH}
-export PYTHONPATH=/lfs/h2/emc/da/noscrub/edward.safford/eva/opt:${PYTHONPATH}
 
 # cd to working directory from which this was launched
 if [ ! -z ${PBS_O_WORKDIR} ]; then cd ${PBS_O_WORKDIR}; fi
