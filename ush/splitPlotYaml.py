3c0e75fc24da0617447c2e9ca257d97e9fc89e3c
edward.safford <edward.safford@noaa.gov>
2024-11-04 21:42:09 +0000

    Ref #49
    Save work in progress

--- ush/splitPlotYaml.py
+++ ush/splitPlotYaml.py
@@ -29,6 +29,40 @@ def removeKey(d, keys):
     return r
 
 
+def check_plotlist(type, logger, plot_list, sat=None, instr=None):
+    """
+    """
+    logger.info(f'--> check_plotlist')
+    sat_reqs = {'rad': ['plot', 'times', 'channels', 'component', 'run'], 
+                'ozn': ['plot', 'times', 'levels', 'component', 'run']} 
+
+    rtn_val = True
+    missing = []
+    match type:
+            case 'sat':   
+                if 'rad' in plot_list.get('plot'):
+                    reqs = sat_reqs.get('rad')
+                else:
+                    reqs = sat_reqs.get('ozn')
+
+                for val in reqs:
+                    if val not in plot_list:
+                        missing.append(val) 
+                        plot_str = (f" {instr} {sat} {plot_list.get('plot')}")         
+
+#           case 'min':   
+#           case 'obs':   
+#           cast _:
+
+    if missing:
+        logger.info(f'WARNING:  YAML for plot {plot_str}  is missing  {missing}')
+        logger.info(f'WARNING:  Requested plot will be SKIPPED.')
+        rtn_val = False
+ 
+    return (rtn_val)
+    logger.info(f'<-- check_plotlist')
+
+
 if __name__ == "__main__":
     """
     splitPlotYaml
@@ -71,6 +105,7 @@ if __name__ == "__main__":
     data = mon_dict.get('data')
 
     if 'satellites' in mon_dict.keys():
+        logger.info(f' split_plot, in satellites')
         sd = removeKey(mon_dict, ['minimization', 'observations'])
 
         for sat in mon_dict.get('satellites'):
@@ -79,14 +114,19 @@ if __name__ == "__main__":
             for inst in sat.get('instruments'):
                 iname = inst.get('name')
                 plist = inst.get('plot_list')
+                for pl in inst.get('plot_list'):
+                    if not check_plotlist('sat', logger, pl, sat=satname, instr=iname):
+                        continue
 
                 # --------------------------------------------------------------------
                 # For instruments with a large number of channels split the plot_list
                 #
                 channels = chan_dict.get(iname)
+                logger.info(f' channels: {channels}')
                 nchans = 0
                 if channels is not None:
                     nchans = len(channels.split(","))
+                logger.info(f' nchans: {nchans}')
 
                 if nchans > 100:
                     ctr = 0
@@ -114,6 +154,11 @@ if __name__ == "__main__":
     if 'minimization' in mon_dict.keys():
         md = removeKey(mon_dict, ['satellites', 'observations'])
         fname = f'OM_PLOT_minimization.yaml'
+        mm = md.get('minimization')
+        logger.info(f'mm: {mm}')
+        for m in mm.get('plot_list'):
+            logger.info(f'm: {m}')
+
         file = open(fname, "w")
         yaml.dump(md, file)
         file.close()
