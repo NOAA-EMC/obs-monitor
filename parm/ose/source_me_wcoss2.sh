48505fd0711257edf29b4ad197099d33bc91c94c
cory martin <cory.r.martin@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2025-04-08 15:29:25 +0000

    add source script

---
+++ parm/ose/source_me_wcoss2.sh
@@ -0,0 +1,8 @@
+module load intel/19.1.3.304
+module use /lfs/h1/mdl/nbm/save/apps/modulefiles
+module load python/3.8.6
+module load python-modules/3.8.6
+module load proj/7.1.0
+module load geos/3.8.1
+module load imagemagick/7.0.8-7
+export PYTHONPATH=$PYTHONPATH:/lfs/h2/emc/da/noscrub/cory.r.martin/emcpy/src
