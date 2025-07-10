ed27f172dc7a20e26a2f22efe4aae06edbf08eaf
Ed Safford <1392868069113305@mil>
2025-07-10 13:13:52 -0400

    Ref #63
    Last of the Windows EOF dragons?  I hope!

--- web/ObsMon/vite.config.js
+++ web/ObsMon/vite.config.js
@@ -9,14 +9,3 @@ export default defineConfig(({ command, mode }) => {
       : '/', // use root path for dev
   };
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: mode === 'production'
-//     ? '/gmb/gdas/radiance/esafford/obsmon/'
-//     : '/', // localhost uses root
-//   plugins: [react()],
-// })
