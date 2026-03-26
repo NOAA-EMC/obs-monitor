4cfb459413404442d4da77b7268b2a56ffba32a7
Edward.Safford <edward.safford@noaa.gov>
2026-03-26 14:22:13 +0000

    Ref #103
    Initial commit to add file push job.

--- dev/workflow/generate_workflow_xml.py
+++ dev/workflow/generate_workflow_xml.py
@@ -134,6 +134,8 @@ def load_config(path: Path) -> Dict[str, Any]:
     cfg["resources"].setdefault("walltime", "00:15:00")
     cfg["resources"].setdefault("task_nodes", "1:ppn=1:tpp=1")
     cfg["resources"].setdefault("task_mem", "4G")
+    cfg["resources"].setdefault("server", "None")
+
     cfg.setdefault("flags", {})
     cfg["flags"].setdefault("copy_data", False)
     cfg["flags"].setdefault("keep_data", False)
@@ -230,11 +232,23 @@ def main() -> None:
         "TASK_NODES": str(cfg["resources"]["task_nodes"]),
         "TASK_MEM": str(cfg["resources"]["task_mem"]),
         "NATIVE": str(cfg.get("resources", {}).get("native", "--export=NONE")),
+
+        "PUSH_QUEUE": str(cfg["hpc"]["push_queue"]),
+        "PUSH_NATIVE": str(cfg.get("resources", {}).get("push_native", "--export=NONE")),
+        "PUSH_WALLTIME": str(cfg["resources"]["push_walltime"]),
+        "PUSH_MEM": str(cfg["resources"]["push_mem"]),
+        "PUSH_NODES": str(cfg["resources"]["push_nodes"]),
+        "PUSH_NATIVE": str(cfg.get("resources", {}).get("push_native", "--export=NONE")),
+        "SERVER": str(cfg["resources"]["server"]),
+        "SERVER_PATH": str(cfg["resources"]["server_path"]),
+        "SERVER_USER": str(cfg["resources"]["server_user"]),
+
         "RUN": str(cfg["run"]),
         "COPY_DATA": bool(cfg["flags"]["copy_data"]),
         "KEEP_DATA": bool(cfg["flags"]["keep_data"]),
         "CREATE_STUBS": bool(cfg["flags"]["create_stubs"]),
         "CYCLES": cfg.get("cycles"),
+        "PUSH_FILES": bool(cfg["flags"]["push_files"]),
     }
 
     # Add MACHINE_ID only if it has a value
---
+++ src/obs_monitor/driver/file_push.py
@@ -0,0 +1,88 @@
+"""
+Observation Monitoring File Push Script 
+
+This script runs an rsync command to push image directories and files
+to a websever.
+
+Algorithm:
+    - Get the current cycle window endpoint from PDY (YYYYMMDD) 
+      and CYC (HH) or CDATE.
+    - Create latestCycle.json file for the current cycle.
+    - Run rsync command to push image directories and files to server
+      while preserving the server's project configiguration files.
+
+Environment (set by Rocoto):
+  PDY (YYYYMMDD), CYC (HH), PSLOT, COMPONENT,
+  RUN, COMROOT, SERVER, SERVER_PATH, SERVER_USER,
+"""
+
+import os
+import subprocess
+import json
+
+from pathlib import Path
+from wxflow import Logger, Jinja
+
+
+def create_latest_cycle_json(path: Path, cdate, logger):
+    output_file = Path(path) / "latestCycle.json"
+    cycle_data = { "cycleTime": str(cdate) }
+
+    try:
+        with open(output_file, 'w', encoding='utf-8') as f:
+            json.dump(cycle_data, f, indent=4)
+        logger.info(f"Successfully created: {output_file}")
+        
+    except Exception as e:
+        logger.error(f"Failed to write JSON: {e}")
+
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+def main():
+    """
+    Parse job list from CONFIG_YAML and run monitoring jobs in parallel.
+    Respects optional COMPONENT filter from the environment.
+    """
+    main_logger = Logger("Obs Monitor File Push - main")
+
+    cdate = os.getenv("CDATE")
+    pdy = os.getenv("PDY")
+    cyc = os.getenv("CYC")
+
+    if not (cdate) and not (pdy and cyc):
+        raise EnvironmentError("CDATE or both PDY and CYC must be set.")
+    
+    if not (cdate):
+        cdate = pdy + cyc
+
+    pslot = os.getenv("PSLOT")
+    component = os.getenv("COMPONENT")
+    comroot = os.getenv("COMROOT")
+    server = os.getenv("SERVER")
+    server_path = os.getenv("SERVER_PATH")
+    server_user = os.getenv("SERVER_USER")
+    run = os.getenv("RUN")
+
+    #--------------------------------------
+    # Add latestCycle.json file to comroot
+    #--------------------------------------
+    create_latest_cycle_json(comroot, cdate, main_logger)
+
+    command = (
+        f"/usr/bin/rsync -ave ssh --update --delete-during "
+        f"--exclude {pslot} --exclude '/atmos' "
+        f"{comroot}/ {server_user}@{server}:{server_path}")
+
+    main_logger.info(f"rsync command: {command}")
+
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    main_logger.info(f"Output: {result.stdout}")
+    main_logger.info(f"Return Code: {result.returncode}") # 0 means success
+
+
+if __name__ == "__main__":
+    main()
